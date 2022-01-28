1967522af48b5c293090b7b587de9a0a6dd17317
PreetRai <prithviraimangya@gmail.com>
2022-01-28 13:55:19 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: Column(
+        children: const <Widget>[
+          Card(
+            child: Text('Chart!'),
+          ),
+          Card(
+            child: Text('list of text'),
+          )
+        ],
+      ),
+    );
+  }
+}
